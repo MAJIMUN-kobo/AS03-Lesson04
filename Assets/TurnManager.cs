1c3bdd0130226c5ac79ebfc3668df3ec89b5e954
majimun <yanbarumajimun@gmail.com>
2023-06-07 13:08:12 +0900

    SEとBGM再生機能の追加

--- Assets/TurnManager.cs
+++ Assets/TurnManager.cs
@@ -20,6 +20,18 @@ public class TurnManager : MonoBehaviour
     // === プレイヤー
     public Player[] player = null;
 
+    // === エフェクト系
+    [Header("=== エフェクト設定 ===")]
+    public GameObject[] effectArray = null;     // エフェクトの配列
+    public GameObject currentEffect = null;     // 現在出しているエフェクト
+
+    // === サウンド系
+    [Header("=== サウンド設定 ===")]
+    public AudioClip[] seArray = null;      // SEの配列
+    public float seVolume = 1;              // SEの音量
+    public AudioClip[] bgmArray = null;     // BGMの配列
+    public float bgmVolume = 1;             // BGMの音量
+
 
     // Start is called before the first frame update
     void Start()
@@ -95,6 +107,15 @@ public class TurnManager : MonoBehaviour
 
         // フェイズを次に進める
         phasePattern = PHASE_HAND_SELECT_P1;
+
+        // プレイヤー1から選択中のエフェクトを出す
+        Vector3 effectPos = player[0].gameObject.transform.position;
+        currentEffect = Instantiate(effectArray[0], effectPos, Quaternion.Euler(-90, 0, 0), player[0].transform);
+
+        // BGMを再生
+        this.GetComponent<AudioSource>().clip = bgmArray[0];
+        //this.GetComponent<AudioSource>().loop = true;
+        this.GetComponent<AudioSource>().Play();
     }
 
     // === P1：手の選択
@@ -102,13 +123,27 @@ public class TurnManager : MonoBehaviour
     {
         Debug.Log("プレイヤー1 >> 手を選んでください。");
 
-        if(Input.GetKeyDown(KeyCode.Alpha1) == true)
+        if (Input.GetKeyDown(KeyCode.Alpha1) == true)
         {   // "1キー"でグーを選択
             player[0].jankenHand = Player.HAND_GU;
 
             Debug.Log("プレイヤー1 >> グーを出しました。");
 
             phasePattern = PHASE_HAND_SELECT_P2;    // フェイズを次に進める
+
+            // エフェクトを削除
+            Destroy(currentEffect.gameObject);
+
+            // プレイヤー2からエフェクトを出す
+            Vector3 effectPos = player[1].transform.position;
+            currentEffect = Instantiate(effectArray[0], effectPos, Quaternion.Euler(-90, 0, 0));
+
+            // プレイヤー1からSEを再生する
+            player[0].GetComponent<AudioSource>().PlayOneShot(seArray[0]);
+
+            // BGMを変える
+            this.GetComponent<AudioSource>().clip = bgmArray[1];
+            this.GetComponent<AudioSource>().Play();
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha2) == true)
@@ -118,6 +153,16 @@ public class TurnManager : MonoBehaviour
             Debug.Log("プレイヤー1 >> チョキを出しました。");
 
             phasePattern = PHASE_HAND_SELECT_P2;    // フェイズを次に進める
+
+            // エフェクトを削除
+            Destroy(currentEffect.gameObject);
+
+            // プレイヤー2からエフェクトを出す
+            Vector3 effectPos = player[1].transform.position;
+            currentEffect = Instantiate(effectArray[0], effectPos, Quaternion.Euler(-90, 0, 0));
+
+            // プレイヤー1からSEを再生する
+            player[0].GetComponent<AudioSource>().PlayOneShot(seArray[0]);
         }
 
         if(Input.GetKeyDown(KeyCode.Alpha3) == true)
@@ -127,6 +172,16 @@ public class TurnManager : MonoBehaviour
             Debug.Log("プレイヤー1 >> パーを出しました。");
 
             phasePattern = PHASE_HAND_SELECT_P2;    // フェイズを次に進める
+
+            // エフェクトを削除
+            Destroy(currentEffect.gameObject);
+
+            // プレイヤー2からエフェクトを出す
+            Vector3 effectPos = player[1].transform.position;
+            currentEffect = Instantiate(effectArray[0], effectPos, Quaternion.Euler(-90, 0, 0));
+
+            // プレイヤー1からSEを再生する
+            player[0].GetComponent<AudioSource>().PlayOneShot(seArray[0]);
         }
     }
 
@@ -142,6 +197,12 @@ public class TurnManager : MonoBehaviour
             Debug.Log("プレイヤー2 >> グーを出しました。");
 
             phasePattern = PHASE_BATTLE;    // フェイズを次に進める
+
+            // エフェクトを削除
+            Destroy(currentEffect.gameObject);
+
+            // プレイヤー2からSEを再生する
+            player[1].GetComponent<AudioSource>().PlayOneShot(seArray[0]);
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha2) == true)
@@ -151,6 +212,12 @@ public class TurnManager : MonoBehaviour
             Debug.Log("プレイヤー2 >> チョキを出しました。");
 
             phasePattern = PHASE_BATTLE;    // フェイズを次に進める
+
+            // エフェクトを削除
+            Destroy(currentEffect.gameObject);
+
+            // プレイヤー2からSEを再生する
+            player[1].GetComponent<AudioSource>().PlayOneShot(seArray[0]);
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha3) == true)
@@ -160,6 +227,12 @@ public class TurnManager : MonoBehaviour
             Debug.Log("プレイヤー2 >> パーを出しました。");
 
             phasePattern = PHASE_BATTLE;    // フェイズを次に進める
+
+            // エフェクトを削除
+            Destroy(currentEffect.gameObject);
+
+            // プレイヤー2からSEを再生する
+            player[1].GetComponent<AudioSource>().PlayOneShot(seArray[0]);
         }
     }
 
@@ -225,10 +298,16 @@ public class TurnManager : MonoBehaviour
         if (player[0].isWinner == true)
         {   // P1の勝利表示
             Debug.Log(player[0].name + "さんの勝ちです。" + player[1].name + "さんに 1 のダメージ！");
+
+            // プレイヤー1からSEを再生する
+            player[0].GetComponent<AudioSource>().PlayOneShot(seArray[1]);
         }
         else if (player[1].isWinner == true)
         {   // P2の勝利表示
             Debug.Log(player[1].name + "さんの勝ちです。" + player[0].name + "さんに 1 のダメージ！");
+
+            // プレイヤー2からSEを再生する
+            player[1].GetComponent<AudioSource>().PlayOneShot(seArray[1]);
         }
 
         // フェイズを次に進める
